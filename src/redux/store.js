dfa7fe3eb33ef38c7a97fb8ee25e149355a78774
Sonika Kumar <sonikak004@gmail.com>
2023-08-19 18:58:59 +0100

    Add store.js to root directory

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+// src/store.js
+
+import { configureStore } from '@reduxjs/toolkit';
+import countriesReducer from './countriesSlice';
+
+const store = configureStore({
+  reducer: {
+    countries: countriesReducer,
+  },
+});
+
+export default store;
